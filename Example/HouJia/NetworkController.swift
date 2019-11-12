e3e5a78b30800013ec7fca5f925252c3aaf696df
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-11-12 21:11:20 +0800

    完成 login 函式。

--- Example/HouJia/NetworkController.swift
+++ Example/HouJia/NetworkController.swift
@@ -22,27 +22,12 @@ extension APIError {
 class NetworkController: NSObject {
   static let shared = NetworkController()
   lazy var requestOperationDictionary = [URL: AsynchronousOperation]()
-//  var current: UInt = 0
-  
-//  func synchronized(_ lock: AnyObject, _ closure: () -> Void) {
-//    objc_sync_enter(lock)
-//    closure()
-//    objc_sync_exit(lock)
-//  }
-//
-//  func next() -> UInt {
-//    synchronized(self) {
-//      current += 1
-//    }
-//    return current
-//  }
   
   lazy var queue: SypQueue = {
     var queue = SypQueue()
     queue.name = "APILoader"
     queue.maxConcurrentOperationCount = 4
     queue.qualityOfService = QualityOfService.userInitiated
-//    queue.addObserver(self, forKeyPath: "operationCount", options: .new, context: nil)
     return queue
   }()
   
@@ -53,7 +38,6 @@ class NetworkController: NSObject {
       return URL(string: "https://express.moshi.com/languages")!
     #endif
   }
-// , completionHandler: @escaping (_ image: UIImage?, _ url: URL) -> Void
   func login(id: String, password: String, type: Int = 0, completionHandler: @escaping ((Result<LoginReponse, APIError>) -> Void)) {
     // 參考：https://express.moshi.com/languages
     let lanCode = "tw"
@@ -76,13 +60,14 @@ class NetworkController: NSObject {
         return
       }
       guard let operation = self.requestOperationDictionary[url] else {
-//        mainThreadCompletionHandler(image: nil, url)
+        logF("operation is not exist. url: \(url)")
         return
       }
       defer {
         self.requestOperationDictionary.removeValue(forKey: url)
       }
       guard operation.isCancelled == false else {
+        logF("operation is isCancelled. url: \(url)")
         // 取消的話就不執行 CompletionHandler
         for dependenceOp in operation.dependencies {
           operation.removeDependency(dependenceOp)
@@ -96,25 +81,19 @@ class NetworkController: NSObject {
         decoder.dateDecodingStrategy = .iso8601
         if let loginReponse = try? decoder.decode(LoginReponse.self, from: response.data) {
           logC(loginReponse)
+          completionHandler(.success(loginReponse))
         } else {
           logF("JSON Decode loginReponse failde")
           assertionFailure()
+          completionHandler(.failure(.unexpectedJSON(httpStatusCode: response.statusCode, json: String(data: response.data, encoding: .utf8) ?? "")))
         }
       case let .failure(error):
         logE(error)
-//        logger.log(error.localizedDescription, theOSLog: .image, level: .error)
-//        mainThreadCompletionHandler(image: nil, url)
+        completionHandler(.failure(.unknown(error: error)))
       }
     }
     requestOperationDictionary[url] = operation
     queue.addOperation(operation)
-    // Response
-//    {
-//        "accessToken": "eyJhbGciOiJSUzI1NiIs...O8whNpZFjNRu1JMfnczDK8oHSUxsoPg",
-//        "expiresIn": 604800,
-//        "requestAt": "2019/9/24 12:45:43"
-//        "tokeyType": "Bearer"
-//    }
   }
   
   func cancelRequest(url: URL) {
@@ -125,33 +104,4 @@ class NetworkController: NSObject {
       }
     }
   }
-  
-//  private func repeatRequiresHandler(url: URL) {
-//    guard requestOperationDictionary[url] == nil else {
-//      let prevoiusOperation = requestOperationDictionary[url]!
-//      let blockOperation = BlockOperation {
-//        DispatchQueue.main.async {
-//          [weak self] in
-//          guard let self = self else {
-//            return
-//          }
-//          guard let image = self.imageCache.object(forKey: url.absoluteString as NSString) else {
-//            fatalError()
-//          }
-//          completionHandler(image, url)
-//        }
-//      }
-//      blockOperation.addDependency(prevoiusOperation)
-//      queue.addOperation(blockOperation)
-//      return
-//    }
-//  }
 }
-
-// MARK: - KVO
-
-//extension ImageLoader {
-//  override func observeValue(forKeyPath _: String?, of _: Any?, change _: [NSKeyValueChangeKey: Any]?, context _: UnsafeMutableRawPointer?) {
-//    logger.log("operationCount: \(queue.operationCount)", theOSLog: .image)
-//  }
-//}
