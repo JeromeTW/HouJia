86caedd2beb856cdc60c8be3629b6387b6bb70ba
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-11-14 09:11:47 +0800

    實作移動 View 當鍵盤遮住 TextField 的時候。

--- HouJia/Classes/Utils/UIViewControllerExtension.swift
+++ HouJia/Classes/Utils/UIViewControllerExtension.swift
@@ -18,3 +18,72 @@ extension UIViewController {
     view.endEditing(true)
   }
 }
+
+// MARK: - Move view with keyboard when keyboard hided textfiled
+
+extension UIViewController {
+  func addKeyboardObserver() {
+    
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(self.keyboardNotifications(notification:)),
+                                           name: UIResponder.keyboardWillChangeFrameNotification,
+                                           object: nil)
+  }
+  
+  func removeKeyboardObserver(){
+    NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+  }
+  
+  // This method will notify when keyboard appears/ dissapears
+  @objc func keyboardNotifications(notification: NSNotification) {
+    
+    var accurateY = 0.0  //Using this we will calculate the selected textFields Y Position
+    
+    if let activeTextField = UIResponder.currentFirst() as? UITextField {
+      // Here we will get accurate frame of which textField is selected if there are multiple textfields
+      let frame = self.view.convert(activeTextField.frame, from:activeTextField.superview)
+      accurateY = Double(frame.origin.y) + Double(frame.size.height)
+    }
+    
+    if let userInfo = notification.userInfo {
+      // here we will get frame of keyBoard (i.e. x, y, width, height)
+      
+      let keyBoardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+      let keyBoardFrameY = keyBoardFrame!.origin.y
+      
+      var newHeight: CGFloat = 0.0
+      //Check keyboards Y position and according to that move view up and down
+      if keyBoardFrameY >= UIScreen.main.bounds.size.height {
+        newHeight = 0.0
+      } else {
+        if accurateY >= Double(keyBoardFrameY) { // if textfields y is greater than keyboards y then only move View to up
+          if #available(iOS 11.0, *) {
+            newHeight = -CGFloat(accurateY - Double(keyBoardFrameY)) - self.view.safeAreaInsets.bottom
+          } else {
+            newHeight = -CGFloat(accurateY - Double(keyBoardFrameY)) - 5
+          }
+        }
+      }
+      //set the Y position of view
+      self.view.frame.origin.y = newHeight
+    }
+  }
+}
+
+public extension UIResponder {
+  
+  private struct Static {
+    static weak var responder: UIResponder?
+  }
+  
+  static func currentFirst() -> UIResponder? {
+    Static.responder = nil
+    UIApplication.shared.sendAction(#selector(UIResponder._trap), to: nil, from: nil, for: nil)
+    return Static.responder
+  }
+  
+  @objc private func _trap() {
+    Static.responder = self
+  }
+}
+
