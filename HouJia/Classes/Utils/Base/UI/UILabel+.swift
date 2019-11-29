0e7acac7a5293e126f1931268d5bf7ce98e43c2c
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-11-29 19:47:09 +0800

    添加 Label 排版函式

--- HouJia/Classes/Utils/Base/UI/UILabel+.swift
+++ HouJia/Classes/Utils/Base/UI/UILabel+.swift
@@ -17,4 +17,35 @@ extension UILabel {
     let lines = Int(textSize.height / charSize)
     return lines
   }
+  
+  public func set(font: UIFont, text: String, withKerning kerning: CGFloat, lineHeight: CGFloat) {
+    self.font = font
+    set(text: text, withKerning: kerning)
+    setLineHeight(lineHeight: lineHeight)
+  }
+
+  public func set(text: String, withKerning kerning: CGFloat) {
+    let attributedString = NSMutableAttributedString(string: text)
+    attributedString.addAttribute(NSAttributedString.Key.kern, value: kerning, range: NSMakeRange(0, text.count))
+
+    attributedText = attributedString
+  }
+
+  public func setLineHeight(lineHeight: CGFloat) {
+    let paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.lineSpacing = 1.0
+    paragraphStyle.lineHeightMultiple = lineHeight
+    paragraphStyle.alignment = textAlignment
+
+    let attrString = NSMutableAttributedString()
+    if attributedText != nil {
+      attrString.append(attributedText!)
+    } else {
+      let notNilText = text ?? ""
+      attrString.append(NSMutableAttributedString(string: notNilText))
+      attrString.addAttribute(NSAttributedString.Key.font, value: font ?? UIFont.systemFont(ofSize: 12), range: NSMakeRange(0, attrString.length))
+    }
+    attrString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attrString.length))
+    attributedText = attrString
+  }
 }
