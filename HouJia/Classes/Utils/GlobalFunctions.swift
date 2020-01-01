e6db2407dc271957d810ab954638b81fdf9e5002
Jerome Hsieh <jerome.developer.tw@gmail.com>
2020-01-01 15:47:00 +0800

    1.添加 Global Func

--- HouJia/Classes/Utils/GlobalFunctions.swift
+++ HouJia/Classes/Utils/GlobalFunctions.swift
@@ -22,3 +22,37 @@ public func getDocumentsDirectory() -> URL {
 public func getTemporaryDirectory() -> URL {
   return URL(fileURLWithPath: NSTemporaryDirectory())
 }
+
+public func generateBoundaryString() -> String {
+  return "Boundary-\(UUID().uuidString)"
+}
+
+public struct PostFileInfo {
+  var filePathKey: String
+  var fileName: String
+  var data: Data
+  var dataType: String
+}
+
+public func createBodyWithParameters(_ parameters: [String: String]?, postFileInfo: PostFileInfo?, boundary: String) -> Data {
+  let body = NSMutableData()
+
+  if let parameters = parameters {
+    for (key, value) in parameters {
+      body.append("--\(boundary)\r\n".data(using: String.Encoding.utf8, allowLossyConversion: true)!)
+      body.append("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".data(using: String.Encoding.utf8, allowLossyConversion: true)!)
+      body.append("\(value)\r\n".data(using: String.Encoding.utf8, allowLossyConversion: true)!)
+    }
+  }
+
+  if let postFileInfo = postFileInfo {
+    body.append("--\(boundary)\r\n".data(using: String.Encoding.utf8, allowLossyConversion: true)!)
+    body.append("Content-Disposition: form-data; name=\"\(postFileInfo.filePathKey)\"; filename=\"\(postFileInfo.fileName)\"\r\n".data(using: String.Encoding.utf8, allowLossyConversion: true)!)
+    body.append("Content-Type: \(postFileInfo.dataType)\r\n\r\n".data(using: String.Encoding.utf8, allowLossyConversion: true)!)
+    body.append(postFileInfo.data)
+    body.append("\r\n".data(using: String.Encoding.utf8, allowLossyConversion: true)!)
+  }
+
+  body.append("--\(boundary)--\r\n".data(using: String.Encoding.utf8, allowLossyConversion: true)!)
+  return body as Data
+}
