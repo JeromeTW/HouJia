b5d5135c2e1523a85a3ea4a756505752cac74b55
Jerome Hsieh <jerome.developer.tw@gmail.com>
2020-03-11 07:55:11 +0800

    關閉 CoordinatedNavigationController 從左往右滑時會返回上一頁的功能

--- HouJia/Classes/Coordinator/CoordinatedNavigationController.swift
+++ HouJia/Classes/Coordinator/CoordinatedNavigationController.swift
@@ -5,6 +5,16 @@
 import UIKit
 
 /// A navigation controller that is aware of its coordinator. This is used extremely rarely through UIResponder-Coordinated.swift, for when we need to find the coordinator responsible for a specific view.
-public class CoordinatedNavigationController: UINavigationController {
+public class CoordinatedNavigationController: UINavigationController, UIGestureRecognizerDelegate {
   public weak var coordinator: Coordinator?
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    interactivePopGestureRecognizer?.delegate = self
+  }
+  
+  public override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+    super.pushViewController(viewController, animated: animated)
+    interactivePopGestureRecognizer?.isEnabled = false
+  }
 }
