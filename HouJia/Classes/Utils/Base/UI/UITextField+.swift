bf928ea199a1ed8d570aac8a3393c5eab251c641
Jerome Hsieh <jerome.developer.tw@gmail.com>
2020-03-15 08:56:32 +0800

    新增 UITextField setInputView 函式

--- HouJia/Classes/Utils/Base/UI/UITextField+.swift
+++ HouJia/Classes/Utils/Base/UI/UITextField+.swift
@@ -5,6 +5,20 @@
 import UIKit
 
 extension UITextField {
+  public func setInputView(view: UIView, doneTitle: String, cancelTitle: String, target: Any, selector: Selector) {
+    // Create a UIDatePicker object and assign to inputView
+    let screenWidth = UIScreen.main.bounds.width
+    inputView = view
+    
+    // Create a toolbar and assign it to inputAccessoryView
+    let toolBar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: screenWidth, height: 44.0))
+    let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+    let cancel = UIBarButtonItem(title: cancelTitle, style: .plain, target: nil, action: #selector(tapCancel))
+    let barButton = UIBarButtonItem(title: doneTitle, style: .plain, target: target, action: selector)
+    toolBar.setItems([cancel, flexible, barButton], animated: false)
+    inputAccessoryView = toolBar
+  }
+  
   public func setInputViewDatePicker(defaultDate: Date = Date(), maximumDate: Date = Date(), doneTitle: String, cancelTitle: String, target: Any, selector: Selector) {
     // Create a UIDatePicker object and assign to inputView
     let screenWidth = UIScreen.main.bounds.width
