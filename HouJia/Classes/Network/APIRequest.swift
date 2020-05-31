54290ef8c81a04bc967913917d22987320c71292
Jerome Hsieh <jerome.developer.tw@gmail.com>
2020-05-31 20:16:06 +0800

    Add some new HTTPMethod

--- HouJia/Classes/Network/APIRequest.swift
+++ HouJia/Classes/Network/APIRequest.swift
@@ -7,6 +7,13 @@ import Foundation
 public enum HTTPMethod: String {
   case get = "GET"
   case post = "POST"
+  case head = "HEAD"
+  case put = "PUT"
+  case delete = "DELETE"
+  case connect = "CONNECT"
+  case options = "OPTIONS"
+  case trace = "TRACE"
+  case patch = "PATCH"
 }
 
 public struct HTTPHeader: Hashable {
